a1988e2e53473405e57f30ed3b0e9965435e083b
José Martín Molina <75451788+josemartinmolina@users.noreply.github.com>
2025-06-26 16:00:54 -0600

    Version inicial

---
+++ uno.py
@@ -0,0 +1,8 @@
+# Programa para determinar si un número es par o impar
+
+numero = int(input("Ingresa un número: "))
+
+if numero % 2 == 0:
+    print(f"{numero} es par.")
+else:
+    print(f"{numero} es impar.")
